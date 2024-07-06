aa21d8be4dd74464c2569d014d1596172d499f25
Muralikrishnakoduru462 <muralikrishnakoduru462@gmail>
2024-07-06 15:59:00 +0530

    episode 01 inception

---
+++ App.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hii murali"),
+    React.createElement("h2", {}, "hii krishna"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hii murali"),
+    React.createElement("h2", {}, "hii krishna"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world from react"
+// );
+
+// console.log(heading); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
